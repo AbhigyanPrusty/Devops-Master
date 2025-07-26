d4a414094e57f4e019689c2c580ebcecaa456524
Abhigyan Prusy <abhigyanprusy@gmail.com>
2025-07-26 23:32:35 +0530

    Fix CodePipeline module to use CodeStar connection

--- modules/codepipeline/main.tf
+++ modules/codepipeline/main.tf
@@ -1,65 +1,18 @@
-resource "aws_codepipeline" "devops_pipeline" {
-  name     = "DevOps2025Pipeline"
-  role_arn = var.pipeline_role_arn
+stage {
+  name = "Source"
 
-  artifact_store {
-    type = "S3"
-    location = "abhigyan-2025"  # same bucket as backend
-  }
+  action {
+    name             = "Source"
+    category         = "Source"
+    owner            = "AWS"
+    provider         = "CodeStarSourceConnection"
+    version          = "1"
+    output_artifacts = ["source_output"]
 
-  stage {
-    name = "Source"
-
-    action {
-      name             = "Source"
-      category         = "Source"
-      owner            = "ThirdParty"
-      provider         = "GitHub"
-      version          = "1"
-      output_artifacts = ["source_output"]
-
-      configuration = {
-        Owner  = var.github_owner
-        Repo   = var.github_repo
-        Branch = var.github_branch
-        OAuthToken = var.github_oauth_token
-      }
-    }
-  }
-
-  stage {
-    name = "Build"
-
-    action {
-      name             = "Build"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      input_artifacts  = ["source_output"]
-      output_artifacts = ["build_output"]
-      version          = "1"
-
-      configuration = {
-        ProjectName = var.codebuild_project_name
-      }
-    }
-  }
-
-  stage {
-    name = "Deploy"
-
-    action {
-      name            = "Deploy"
-      category        = "Deploy"
-      owner           = "AWS"
-      provider        = "CodeDeploy"
-      input_artifacts = ["build_output"]
-      version         = "1"
-
-      configuration = {
-        ApplicationName     = var.codedeploy_app_name
-        DeploymentGroupName = var.codedeploy_group_name
-      }
+    configuration = {
+      ConnectionArn    = "arn:aws:codeconnections:ap-south-1:710783459512:connection/ace5dbe2-b79b-44cf-b008-3518d854fdbe"
+      FullRepositoryId = "AbhigyanPrusty/Devops-Master"
+      BranchName       = "main"
     }
   }
 }
